27ab9766542cc55a43aaeb78e037aacaf65afadd
Krzysztof Wolski <krzychu@macbook-krzysztof-wolski.local>
2011-04-23 20:41:09 +0200

    Initial Commit

---
+++ labicos2d/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  labicos2d
+//
+//  Created by Krzysztof Wolski on 11-04-23.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ labicos2d/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  labicos2d
+//
+//  Created by Krzysztof Wolski on 11-04-23.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
